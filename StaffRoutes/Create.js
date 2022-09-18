f67b71d13e5aec299e60502b6988248101296973
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-09-18 14:26:46 +0200

    few changes

--- StaffRoutes/Create.js
+++ StaffRoutes/Create.js
@@ -18,14 +18,12 @@ const router = express.Router();
 router.post("/CreateMenu", async (req, res) => {
   const { data, type } = req.body;
 
-  for (let i = 0; i < data.length; i++) {
-    const curr = data[i];
-    const ref = doc(db, `Dining/OriginalMenu/${type}`, uid(10));
-    // console.log(curr)
+  const ref = doc(db,"Dining",type);
 
-    await setDoc(ref, curr);
-  }
+  let temp = {};
 
+  await setDoc(ref,data);
+  
   res.send("Done");
 });
 
@@ -36,7 +34,7 @@ router.post("/AddDiningHalls", async (req, res)=>{
   for (let i = 0; i < data.length; i++) {
     const curr = data[i];
     const ref = doc(db, `Dining/DiningHalls/DiningHallNames`, curr.id);
-    await setDoc(ref, curr);
+    await setDoc(ref, {...curr,breakfast:{},lunch:{},dinner:{}});
   }
 
   res.send("Done");
